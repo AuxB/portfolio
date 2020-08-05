451c5f8ca387602f8e74b4f7ce2f55e5f6884b7e
Auxence <auxence.blondel@gmail.com>
2020-08-05 12:10:03 +0200

    configuration of eslint

--- client/src/components/WorkCard.jsx
+++ client/src/components/WorkCard.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
 import './WorkCard.css';
-import githubLogo from '../img/github.svg';
+import githubLogo from '../img/github.svg'
 
 function WorkCard(props) {
-    const { work } = props
-    return (
-        <div className="WorkCard">
-            <img src={work.screenshot} alt="thumb" />
-            <div className="languages-logo">
-                {work.languages.map(language => {
-                    return <img src= {`/images/logo/${language}.png`
-                } />
-            })}
-            </div>
-            <div className="text-card">
-                <h3>{work.name}</h3>
-                <p>{work.description}</p>
-            </div>
-            <div className="view-section">
-                {work.url ? <button><a href='#'>Voir</a></button>: <button disabled><a>Voir</a></button> }
-                <img src={githubLogo} alt="github logo" />
-            </div>
-        </div>
-    );
+  const { work } = props;
+  return (
+    <div className="WorkCard">
+      <img src={work.screenshot} alt="thumb" />
+      <div className="languages-logo">
+        {work.languages.map(language => {
+          return <img src={`/images/logo/${language}.png`
+          } />
+        })}
+      </div>
+      <div className="text-card">
+        <h3>{work.name}</h3>
+        <p>{work.description}</p>
+      </div>
+      <div className="view-section">
+        {work.url ? <button><a href='#'>Voir</a></button> : <button disabled><a>Voir</a></button>}
+        <img src={githubLogo} alt="github logo" />
+      </div>
+    </div>
+  );
 }
 
 export default WorkCard;
